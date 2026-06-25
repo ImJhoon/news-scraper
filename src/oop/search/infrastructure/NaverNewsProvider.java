50021effaa47a6d5e9c47181791d5960c7fd25e9
ImJhoon <jhjh9907@gmail.com>
2026-06-25 14:24:59 +0900

    feat: NaverNewsProvider 응답 JSON 수동 파싱 및 도메인 객체 변환 로직 추가

--- src/oop/search/infrastructure/NaverNewsProvider.java
+++ src/oop/search/infrastructure/NaverNewsProvider.java
@@ -9,6 +9,8 @@ import java.net.URLEncoder;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class NaverNewsProvider extends AbstractHttpScraper {
@@ -46,6 +48,9 @@ public class NaverNewsProvider extends AbstractHttpScraper {
                 .header("X-Naver-Client-Id", clientId)
                 .header("X-Naver-Client-Secret", clientSecret)
                 .build();
+        
+        List<NewsResult> results = new ArrayList<>();
+        
         try {
             HttpResponse<String> response = httpClient.send(
                     request,
@@ -53,16 +58,47 @@ public class NaverNewsProvider extends AbstractHttpScraper {
             );
             String body = response.body();
             System.out.println("body = " + body);
+            
+            // items 
+            String items = body.split("items")[1];
+            // <- items -> 
+            System.out.println("items = " + items);
+            String[] itemArr = items.split("},");
+            
+            for (String item : itemArr) {
+//                String title = item
+//                        .split("\"title\":\"")[1]
+//                        .split("\",")[0];
+                String title = cutText(item, "\"title\":\"", "\",");
+                String link = cutText(item, "\"link\":\"", "\",");
+                String description = cutText(item, "\"description\":\"", "\",");
+                String pubDate = "";
+//                System.out.println("title = " + title);
+//                System.out.println("description = " + description);
+                NewsResult result = new NewsResult(title, description, link, pubDate);
+                results.add(result);
+            }
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return List.of();
+//        return List.of();
+        return results;
+    }
+
+    public String cutText(String original, String prefix, String suffix) {
+        return original
+                .split(prefix)[1]
+                .split(suffix)[0];
     }
 
     public static void main(String[] args) {
         NewsProvider provider = new NaverNewsProvider();
-        List<NewsResult> results = provider.fetchNews("북중미 월드컵 한국", 10);
-        System.out.println("results = " + results);
+        List<NewsResult> results = provider.fetchNews("주식", 10);
+//        System.out.println("results = " + results);
+        for (NewsResult newsItem : results) {
+            System.out.println("newsItem = " + newsItem);
+        }
     }
 }
